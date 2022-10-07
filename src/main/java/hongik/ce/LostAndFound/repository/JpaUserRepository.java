f4900dcc8d3847b347c18c97bb51a3b5e7bbdc41
YooByeongIk <quddlr96@naver.com>
2022-10-07 17:38:47 +0900

    Auto stash before revert of "[SETTING] Save User Test"

--- src/main/java/hongik/ce/LostAndFound/repository/JpaUserRepository.java
+++ src/main/java/hongik/ce/LostAndFound/repository/JpaUserRepository.java
@@ -1,18 +1,7 @@
 package hongik.ce.LostAndFound.repository;
 
-<<<<<<< Updated upstream
-import lombok.RequiredArgsConstructor;
-import org.apache.catalina.session.PersistentManager;
-
-@RequiredArgsConstructor
-public class JpaUserRepository{
-
-    private final PersistentManager em;
-
-=======
 import hongik.ce.LostAndFound.domain.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaUserRepository extends JpaRepository<User,Long> {
->>>>>>> Stashed changes
 }
