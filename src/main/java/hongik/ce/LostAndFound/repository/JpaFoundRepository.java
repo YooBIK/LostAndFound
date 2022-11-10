4b639218894dae6e9cc9e9fc4f781b0cbac6a2dd
YooByeongIk <quddlr96@naver.com>
2022-11-10 14:14:15 +0900

    [MERGE]

--- src/main/java/hongik/ce/LostAndFound/repository/JpaFoundRepository.java
+++ src/main/java/hongik/ce/LostAndFound/repository/JpaFoundRepository.java
@@ -1,4 +1,23 @@
 package hongik.ce.LostAndFound.repository;
 
-public interface JpaFoundRepository {
+import hongik.ce.LostAndFound.domain.entity.Category;
+import hongik.ce.LostAndFound.domain.entity.Found;
+import hongik.ce.LostAndFound.domain.entity.Lost;
+import hongik.ce.LostAndFound.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface JpaFoundRepository extends JpaRepository<Found,Long> {
+
+
+    Found findByFoundId(Long FoundId);
+
+    @Modifying
+    @Query(value = "UPDATE Found l SET l.hit = l.hit+1 WHERE l.foundId = :foundId")
+    int updateHit(@Param(value = "foundId") Long foundId);
+    List<Found> findAllByUser(User user);
 }
